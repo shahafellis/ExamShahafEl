99157acc532c9455e94a3dd419aa8e29c2c3c7d7
shahafellis <amziki123@gmail.com>
2020-06-09 22:12:24 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ExamShahafEl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.putNumTxtBx = new System.Windows.Forms.TextBox();
+            this.resultLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // putNumTxtBx
+            // 
+            this.putNumTxtBx.Location = new System.Drawing.Point(106, 76);
+            this.putNumTxtBx.Name = "putNumTxtBx";
+            this.putNumTxtBx.Size = new System.Drawing.Size(100, 20);
+            this.putNumTxtBx.TabIndex = 0;
+            this.putNumTxtBx.TextChanged += new System.EventHandler(this.putNumTxtBx_TextChanged);
+            
+            // 
+            // resultLbl
+            // 
+            this.resultLbl.AutoSize = true;
+            this.resultLbl.Location = new System.Drawing.Point(252, 83);
+            this.resultLbl.Name = "resultLbl";
+            this.resultLbl.Size = new System.Drawing.Size(13, 13);
+            this.resultLbl.TabIndex = 1;
+            this.resultLbl.Text = "?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLbl);
+            this.Controls.Add(this.putNumTxtBx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox putNumTxtBx;
+        private System.Windows.Forms.Label resultLbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamShahafEl
+{
+    public partial class Form1 : Form
+    {
+       
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void putNumTxtBx_TextChanged(object sender, EventArgs e)
+        {
+            int b = 0;
+            bool a = Int32.TryParse(putNumTxtBx.Text, out b);
+            if (b == 0)
+            {
+                resultLbl.Text = "zero";
+
+            }
+            else if (b < 0)
+            {
+
+
+
+                resultLbl.Text = "Negative";
+
+            }
+
+
+            else
+            {
+                resultLbl.Text = "Positive";
+
+            }
+
+            if (Int32.TryParse(putNumTxtBx.Text, out b) == false)
+            {
+
+                resultLbl.Text = "Nan";
+            }
+
+
+            
+              
+            
+            
+            
+
+            
+            
+
+            
+                
+
+                
+        
+        }
+
+        
+    }
+}
